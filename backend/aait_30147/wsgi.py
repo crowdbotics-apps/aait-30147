23f8c2eefdbb7a0cc40eba6fded2e799a47f6eb6
Crowdbotics <team@crowdbotics.com>
2021-08-27 12:01:21 +0000

    Update models

--- backend/aait_30147/wsgi.py
+++ backend/aait_30147/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aait_30147.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aait_30147.settings")
 
 application = get_wsgi_application()
